24482a707f6c55112cc458138ae9e83556ab7488
gitdoallan <mailtoallan@icloud.com>
2022-04-19 05:20:15 -0300

    removendo router-dom

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-import NotFound from '../pages/NotFound';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ Home } />
-        <Route component={ NotFound } />
-      </Switch>
-    </BrowserRouter>
+    <Home />
   );
 }
